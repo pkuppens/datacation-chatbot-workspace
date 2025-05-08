a391a70495a008c3ee536c31b23830a793869d42
Pieter Kuppens <pieter.kuppens@gmail.com>
2025-05-08 21:05:34 +0200

    remove old titanic tool

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,5 +1,3 @@
-from .database import titanic_db
-from .titanic_tool import titanic_tool
 from .titanic_pandas_tool import titanic_pandas_tool
 
-__all__ = ['titanic_db', 'titanic_tool', 'titanic_pandas_tool'] 
+__all__ = ['titanic_pandas_tool'] 
